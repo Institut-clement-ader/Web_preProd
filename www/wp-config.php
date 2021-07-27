54b4c597f9e03938ac1197160547814620511150
TM <tmangeard@gmx.fr>
2021-07-27 15:52:23 +0200

    parameters debug: false

--- www/wp-config.php
+++ www/wp-config.php
@@ -76,7 +76,7 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 ///* DEBUG
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 define('WP_DEBUG_LOG', false);
 define('WP_DEBUG_DISPLAY', false);
 define('FORCE_SSL_ADMIN', false);
